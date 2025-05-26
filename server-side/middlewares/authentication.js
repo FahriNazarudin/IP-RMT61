0c10a23b7c0060b2c1669beb6b3289480cb0fa97
Fahri Nazarudin <fahrinzrdn@Fahris-MacBook-Pro.local>
2025-05-26 21:35:19 +0700

    add file seeder json

---
+++ server-side/middlewares/authentication.js
@@ -0,0 +1,32 @@
+const { verifyToken } = require("../helpers/jwt");
+const { User } = require("../models");
+
+async function authentication(req, res, next) {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      throw { statusCode: 401, message: "Token required" };
+    }
+
+    const token = authorization.split(" ")[1];
+    const payload = verifyToken(token);
+
+    const user = await User.findByPk(payload.id);
+    if (!user) {
+      throw { statusCode: 401, message: "Invalid token" };
+    }
+
+    req.user = {
+      id: user.id,
+      email: user.email,
+      displayName: user.displayName,
+    };
+
+    next();
+  } catch (error) {
+    next({ statusCode: 401, message: "Invalid token" });
+  }
+}
+
+module.exports = authentication;
