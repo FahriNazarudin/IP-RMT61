b21290d6d474c36052992de0634740a989f54f74
Fahri Nazarudin <fahrinzrdn@Fahris-MacBook-Pro.local>
2025-05-27 19:46:17 +0700

    feat: get item movie to watchlist

--- server-side/controllers/WatchlistController.js
+++ server-side/controllers/WatchlistController.js
@@ -1,15 +1,14 @@
 const { Watchlist, Movie } = require("../models");
 
-
 module.exports = class WatchlistController {
   static async addToWatchlist(req, res, next) {
     const { movieId } = req.body;
     const userId = req.user.id;
     if (!movieId) {
-      throw { name : "NotFound" , message: "Movie id is required" };
+      throw { name: "NotFound", message: "Movie id is required" };
     }
     if (!userId) {
-      throw { name : "NotFound" , message: "user id is required" };
+      throw { name: "NotFound", message: "user id is required" };
     }
     try {
       const watchlistItem = await Watchlist.create({
@@ -25,4 +24,27 @@ module.exports = class WatchlistController {
       next(error);
     }
   }
-}
+
+
+  static async getWatchlist(req, res, next) {
+    const userId = req.user.id;
+    try {
+      const watchlistItems = await Watchlist.findAll({
+        where: { user_id: userId },
+        include: {
+          model: Movie,
+            attributes: {
+                exclude: ["createdAt", "updatedAt"],
+            },
+        },
+      });
+
+      return res.status(200).json(watchlistItems);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+
+  
+};
