c201d1fdd2dac0aba272807b175b665a920cd903
Fahri Nazarudin <fahrinzrdn@Fahris-MacBook-Pro.local>
2025-05-29 22:44:41 +0700

    debugging

--- server-side/app.js
+++ server-side/app.js
@@ -16,6 +16,7 @@ const AIController = require("./controllers/AIController");
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
